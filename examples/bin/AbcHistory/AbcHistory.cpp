feacf530a56a6e8499ebfd29f2214b1e924aa95f
Marco Pantaleoni <marco.pantaleoni@gmail.com>
2015-05-22 18:12:04 +0200

    Add to 'abchistory' utility the ability to skip reading archive contents.
    (cherry picked from commit 4ef956bf41e9fd697d30b47674f284629d0f705e)

--- examples/bin/AbcHistory/AbcHistory.cpp
+++ examples/bin/AbcHistory/AbcHistory.cpp
@@ -46,46 +46,89 @@
 
 static void usage()
 {
-    printf ("Usage: abchistory file\n");
-    printf ("Used to list modification history of an Alembic file (using the Git backend)\n\n");
+    printf ("Usage: abchistory [-r | --read] FILE...\n");
+    printf ("Used to list modification history of one or more Alembic files.\n\n");
+    printf ("Specified files must be using the Git backend.\n");
+    printf ("If -r is not provided the Alembic file is not parsed.\n");
+    printf ("Options:\n\n");
+    printf ("  -r | --read   Read and process the archive.\n");
 
     exit(EXIT_FAILURE);
 }
 
+static bool hasOption(const std::vector<std::string>& options, const std::string& option)
+{
+    std::vector<std::string>::const_iterator it;
+    for (it = options.begin(); it != options.end(); ++it)
+    {
+        const std::string& it_option = *it;
+        if (it_option == option)
+            return true;
+    }
+    return false;
+}
+
 int main(int argc, char *argv[])
 {
-    std::string inFile;
-
-    int arg_i = 1;
-
-    if (argc != 2)
+    if (argc < 2)
         usage();
 
-    inFile = argv[arg_i++];
+    // parse args
+    std::vector<std::string> arguments(argv, argv + argc);
+    std::vector<std::string> options;
+    std::vector<std::string> files;
 
-    Alembic::AbcCoreFactory::IOptions rOptions;
-
-    Alembic::AbcCoreFactory::IFactory factory;
-    Alembic::AbcCoreFactory::IFactory::CoreType coreType;
-    Alembic::Abc::IArchive archive = factory.getArchive(inFile, coreType, rOptions);
-    if (!archive.valid())
+    // separate file args from option args
+    for (std::size_t i = 1; i < arguments.size(); i++)
     {
-        printf("Error: Invalid Alembic file specified: %s\n",
-               inFile.c_str());
-        return 1;
+        if (arguments[i].substr(0, 1) == "-")
+            options.push_back(arguments[i]);
+        else
+            files.push_back(arguments[i]);
     }
 
-    if (coreType != Alembic::AbcCoreFactory::IFactory::kGit)
+    if (files.size() < 1)
+        usage();
+
+    bool opt_read_archive = hasOption(options, "-r") || hasOption(options, "--read");
+
+    std::vector<std::string>::const_iterator it;
+    for (it = files.begin(); it != files.end(); ++it)
     {
-        printf("Warning: Alembic file specified: %s\n", inFile.c_str());
-        printf("is not of the required Git type.\n");
-        return 1;
+        const std::string& file = *it;
+
+        std::string jsonHistory;
+
+        if (opt_read_archive)
+        {
+            Alembic::AbcCoreFactory::IOptions rOptions;
+
+            Alembic::AbcCoreFactory::IFactory factory;
+            Alembic::AbcCoreFactory::IFactory::CoreType coreType;
+            Alembic::Abc::IArchive archive = factory.getArchive(file, coreType, rOptions);
+            if (!archive.valid())
+            {
+                std::cerr << "ERROR: Invalid Alembic file specified: " << file << std::endl;
+                exit(EXIT_FAILURE);
+            }
+
+            if (coreType != Alembic::AbcCoreFactory::IFactory::kGit)
+            {
+                std::cerr << "ERROR: Alembic file specified: " << file << " not of the required type (Git)." << std::endl;
+                exit(EXIT_FAILURE);
+            }
+
+            bool error = false;
+            jsonHistory = Alembic::AbcCoreGit::getHistoryJSON(archive, error);
+        } else
+        {
+            bool error = false;
+            jsonHistory = Alembic::AbcCoreGit::getHistoryJSON(file, error);
+        }
+
+        std::cout << "Alembic file '" << file << "' (JSON) history:" << std::endl;
+        std::cout << jsonHistory << std::endl;
     }
 
-    bool error = false;
-    std::string jsonHistory = Alembic::AbcCoreGit::getHistoryJSON(archive, error);
-
-    std::cout << jsonHistory << std::endl;
-
-    return 0;
+    exit(EXIT_SUCCESS);
 }
