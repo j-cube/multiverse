fbcd795359de76fce3f50d96c2b3b4aaf7eb2b76
Marco Pantaleoni <marco.pantaleoni@gmail.com>
2015-07-20 18:06:39 +0200

    Add proper copyright notices.

--- lib/Alembic/AbcCoreGit/CprImpl.cpp
+++ lib/Alembic/AbcCoreGit/CprImpl.cpp
@@ -1,10 +1,25 @@
-//-*****************************************************************************
-//
-// Copyright (c) 2014,
-//
-// All rights reserved.
-//
-//-*****************************************************************************
+/*****************************************************************************/
+/*  Copyright (c) 2013-2015 J CUBE I. Tokyo, Japan. All Rights Reserved.     */
+/*                                                                           */
+/*  This program is free software; you can redistribute it and/or modify     */
+/*  it under the terms of the GNU General Public License as published by     */
+/*  the Free Software Foundation; either version 2 of the License, or        */
+/*  (at your option) any later version.                                      */
+/*                                                                           */
+/*  This program is distributed in the hope that it will be useful,          */
+/*  but WITHOUT ANY WARRANTY; without even the implied warranty of           */
+/*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the            */
+/*  GNU General Public License for more details.                             */
+/*                                                                           */
+/*  You should have received a copy of the GNU General Public License along  */
+/*  with this program; if not, write to the Free Software Foundation, Inc.,  */
+/*  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.              */
+/*                                                                           */
+/*  J CUBE Inc.                                                              */
+/*  6F Azabu Green Terrace                                                   */
+/*  3-20-1 Minami-Azabu, Minato-ku, Tokyo                                    */
+/*  info@-jcube.jp                                                           */
+/*****************************************************************************/
 
 #include <Alembic/AbcCoreGit/CprImpl.h>
 
