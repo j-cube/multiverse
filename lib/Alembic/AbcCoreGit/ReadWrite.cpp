a4efa48068e6512be9929c0743a81233f1c786d6
Paolo Berto Durante <pberto@gmail.com>
2015-07-23 13:04:31 +0900

    updated license in all sources

--- lib/Alembic/AbcCoreGit/ReadWrite.cpp
+++ lib/Alembic/AbcCoreGit/ReadWrite.cpp
@@ -1,9 +1,10 @@
 /*****************************************************************************/
-/*  Copyright (c) 2013-2015 J CUBE I. Tokyo, Japan. All Rights Reserved.     */
+/*  multiverse - a next generation storage back-end for Alembic              */
+/*  Copyright (C) 2015 J CUBE Inc. Tokyo, Japan. All Rights Reserved.        */
 /*                                                                           */
-/*  This program is free software; you can redistribute it and/or modify     */
+/*  This program is free software: you can redistribute it and/or modify     */
 /*  it under the terms of the GNU General Public License as published by     */
-/*  the Free Software Foundation; either version 2 of the License, or        */
+/*  the Free Software Foundation, either version 3 of the License, or        */
 /*  (at your option) any later version.                                      */
 /*                                                                           */
 /*  This program is distributed in the hope that it will be useful,          */
@@ -11,14 +12,13 @@
 /*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the            */
 /*  GNU General Public License for more details.                             */
 /*                                                                           */
-/*  You should have received a copy of the GNU General Public License along  */
-/*  with this program; if not, write to the Free Software Foundation, Inc.,  */
-/*  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.              */
+/*  You should have received a copy of the GNU General Public License        */
+/*  along with this program.  If not, see <http://www.gnu.org/licenses/>.    */
 /*                                                                           */
-/*  J CUBE Inc.                                                              */
-/*  6F Azabu Green Terrace                                                   */
-/*  3-20-1 Minami-Azabu, Minato-ku, Tokyo                                    */
-/*  info@-jcube.jp                                                           */
+/*    J CUBE Inc.                                                            */
+/*    6F Azabu Green Terrace                                                 */
+/*    3-20-1 Minami-Azabu, Minato-ku, Tokyo                                  */
+/*    info@-jcube.jp                                                         */
 /*****************************************************************************/
 
 #include <Alembic/AbcCoreGit/ReadWrite.h>
