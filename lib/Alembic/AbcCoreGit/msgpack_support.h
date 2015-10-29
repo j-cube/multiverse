1b5578f16a4837967cd951fe0a3261155fce5051
Paolo Berto Durante <pberto@gmail.com>
2015-10-29 12:56:14 +0900

    changed license from GPLv3 to Apache 2.0

--- lib/Alembic/AbcCoreGit/msgpack_support.h
+++ lib/Alembic/AbcCoreGit/msgpack_support.h
@@ -1,24 +1,19 @@
 /*****************************************************************************/
 /*  multiverse - a next generation storage back-end for Alembic              */
-/*  Copyright (C) 2015 J CUBE Inc. Tokyo, Japan. All Rights Reserved.        */
 /*                                                                           */
-/*  This program is free software: you can redistribute it and/or modify     */
-/*  it under the terms of the GNU General Public License as published by     */
-/*  the Free Software Foundation, either version 3 of the License, or        */
-/*  (at your option) any later version.                                      */
+/*  Copyright 2015 J CUBE Inc. Tokyo, Japan.                                 */
 /*                                                                           */
-/*  This program is distributed in the hope that it will be useful,          */
-/*  but WITHOUT ANY WARRANTY; without even the implied warranty of           */
-/*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the            */
-/*  GNU General Public License for more details.                             */
+/*  Licensed under the Apache License, Version 2.0 (the "License");          */
+/*  you may not use this file except in compliance with the License.         */
+/*  You may obtain a copy of the License at                                  */
 /*                                                                           */
-/*  You should have received a copy of the GNU General Public License        */
-/*  along with this program.  If not, see <http://www.gnu.org/licenses/>.    */
+/*      http://www.apache.org/licenses/LICENSE-2.0                           */
 /*                                                                           */
-/*    J CUBE Inc.                                                            */
-/*    6F Azabu Green Terrace                                                 */
-/*    3-20-1 Minami-Azabu, Minato-ku, Tokyo                                  */
-/*    info@-jcube.jp                                                         */
+/*  Unless required by applicable law or agreed to in writing, software      */
+/*  distributed under the License is distributed on an "AS IS" BASIS,        */
+/*  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. */
+/*  See the License for the specific language governing permissions and      */
+/*  limitations under the License.                                           */
 /*****************************************************************************/
 
 #ifndef _Alembic_AbcCoreGit_msgpack_support_h_
